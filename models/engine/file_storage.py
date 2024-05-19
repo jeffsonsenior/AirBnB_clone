fe311fae5fce2ef9c5e124c3a4b28bf18c6422a3
jeffsonsenior <kinyuajeff@gmail.com>
2024-05-19 06:52:11 +0000

    file storage module

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,7 +3,7 @@
 Defines the FileStorage class
 """
 import json
-from medels.base_model import BaseModel
+from models.base_model import BaseModel
 from datetime import datetime
 from models.user import User
 from models.state import State
@@ -11,17 +11,76 @@ from models.city import City
 from model.amenity import Amenity
 from model.review import Review
 from model.place import Place
+import os.path
 
 class FileStorage:
+    """Abstracted storage engine.
+    Attributes:
+        __file_path (str): json file path
+        __objects (dict): dictionary where object will be stored
+    """
     __file_path = "file.path"
     __objects = {}
+    def __init__(self):
+        """Initialize methode"""
+        pass
 
     def all(self):
-        returm FileStorage.__objects
+        """
+        Returns the objct dict
+        provide access to all stored obj
+        """
+        self.reload()
+        return self.__objects
+
 
     def save(self):
-        with open(FileStorage.__filePath,
-                  mode="w",
-                  encoding+"utf-8") as f:
-            json_str = json.dumps(FileStore._objects,
-                                  default=     
+        """
+        Serializes ojct dict into
+        Json format and save it to __file_path
+        """
+        new_dict = {}
+        for key, value in self._objects.items():
+            new_dict[key] = value.to_dict()
+        with open(self.__file_path, "w") as f:
+            json.dump(new_dict, f)
+
+    def reload(self):
+        """
+        Deserialising the Json file to object
+        """
+
+        try:
+            if os.path.isfile(self.__file_path):
+                with open(self.__file_path, "r") as f:
+                    for key, value in json.load(f).items():
+                        self.__objects[key] = eval(value["__class__"])(**value)
+        except FileNotFoundError:
+            return
+        except json.JSONDedeError:
+            return
+
+    def delete(self, obj=None):
+        """
+        deleting obj from __objects if present
+        """
+        if obj is None:
+            return
+        if obj in self.__objects:
+            del self.__objects[obj]
+        self.save()       
+
+    def update(self, obj, k, v):
+        """
+        updating obj withk:v as attributes
+        """
+        if obj in self.__objects:
+           setattr(self.__objects[obj], k, eval(v))
+        self.save()
+
+    def getid(self, obj):
+        """
+        Return the id obj
+        """
+        if obj in self.__objects:
+            return self.__objects[obj].id
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+"""
+Module for BaseModel unittest
+"""
+import unittest
+import uuid
+import json
+import os
+import models
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+
+   def __init__(self, *args, **kwargs):
+      """ Initializing the BaseModel """
+        super().__init__(*args, **kwargs)
+
+        self.name = "BaseModel"
+        self.value = BaseModel
+
+    def setUp(self):
+        """
+        Set up method to prepare each test
+        """
+        self.model = BaseModel()
+
+    def test_instance(self):
+        """
+        Test if the created object is an instance of BaseModel
+        """
+        self.assertIsInstance(self.model, BaseModel)
+
+ def tearDown(self):
+      """
+      temporary files path
+      """
+      try:
+         os.remove('file.json')
+      except FileNotFoundError:
+          pass
+      try:
+         os.rename("tmp.json", "file.json")
+      except FileNotFoundError:
+
+   def test_base_model_docstrings(self):
+     """
+     test for docstring method
+     """
+       self.assertIsNotNone(BaseModel.__doc__)
+       self.assertIsNotNone(BaseModel.__init__.__doc__)
+       self.assertIsNotNone(BaseModel.__str__.__doc__)
+       self.assertIsNotNone(BaseModel.save.__doc__)
+       self.assertIsNotNone(BaseModel.to__dict.__doc__)
+
+   def test_Isinstanceof(self):
+      self.assertIsInstance(bm, BaseModel)
+      self.assertIsInstance(bm.id, str)
+      self.assertIsInstance(bm.created_at, datetime)
+      self.assertIsInstance(bm.updated_at, datetime)
+
+   def test_id(self):
+      """
+      test if a new id is created
+      """
+      bm = BaseModel()
+      bm2 = BaseModel()
+      self.assertNotEqual(bm.id, bm2.id)
+   
+   def test_base_model_str(self):
+      bm = BaseModel()
+      self.assertEqual(str(bm), "[BaseModel] ({}) {}".format(bm.id, bm.__dict__))
+
+   def test_basemodel_save(self):
+      bm = BaseModel()
+      created_at = bm.created_at
+      bm.save()
+      self.assertEqual(bm.created_at, created_at)
+      self.assertNotEqual(bm.updated_at, created_at)
+
+   def test_basemodel_to_dict(self):
+      bm = BaseModel()
+      bm_dict = bm.to_dict()
+      self.assetIsInstance(bm_dict, dict)
+      self.assertEqual(bm_dict["id"], bm.id)
+      self.assertEqual(bm_dict["created_at"], bm.created_at.isoformat())
+      self.assertEqual(bm_dict["updated_at"], bm.updated_at.isoformat())
+      self.assertEqual(bm_dict["__class__"], "BaseModel")
+
+  if __name__ == '__main__':
+      unittest.main()
